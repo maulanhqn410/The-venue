0e1e4b3f1be44f8f4e4f4173316834f8b09df86a
anh <maulanhqn1234@gmail.com>
2019-12-30 17:10:37 +0700

    finishing the header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -5,25 +5,42 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import SideDrawer from "./SideDrawer";
 
-
 export default class Header extends Component {
   state = {
     drawerOpen: false,
-  }
+    headerShower: true
+  };
 
-  toggleDrawer(value){
+  toggleDrawer(value) {
     this.setState({
       drawerOpen: value
-    })
+    });
   }
 
+  componentDidMount(){
+    window.addEventListener("scroll", this.handleCroll);
+  }
+
+  handleCroll = () => {
+    if(window.scrollY > 0){
+      this.setState({
+        headerShower: false
+      })
+    } else{
+      this.setState({
+        headerShower: true
+      })
+    }
+  }
   render() {
     return (
       <div>
         <AppBar
           position="fixed"
           style={{
-            backgroundColor: "#2f2f2f",
+            backgroundColor: this.state.headerShower
+              ? "#2f2f2f"
+              : "transparent",
             boxShadow: "none",
             padding: "10px 0"
           }}
@@ -34,12 +51,19 @@ export default class Header extends Component {
               <div className="header_logo_title">Musical events</div>
             </div>
 
-            <IconButton aria-label="menu" color="inherit" onClick={() => this.toggleDrawer(true)}>
-              <MenuIcon/>
+            <IconButton
+              aria-label="menu"
+              color="inherit"
+              onClick={() => this.toggleDrawer(true)}
+            >
+              <MenuIcon />
             </IconButton>
           </Toolbar>
 
-          <SideDrawer open={this.state.drawerOpen} onClose={(value) => this.toggleDrawer(value)}/>
+          <SideDrawer
+            open={this.state.drawerOpen}
+            onClose={value => this.toggleDrawer(value)}
+          />
         </AppBar>
       </div>
     );
